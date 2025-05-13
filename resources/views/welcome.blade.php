09b2f2f607a0d0b29d20dd38978358b14ede66f8
bilal544 <mughalzadaitx744@gmail.com>
2025-05-13 17:12:43 +0500

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1 class="text-red-600 text-5xl capitalize font-semibold">
+        laravel app
+    </h1>
+</body>
+</html>

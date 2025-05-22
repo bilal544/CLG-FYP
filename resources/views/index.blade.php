8f6e3eaad72e2ff0c4dd8c6a3fa64b180bd2d6ec
bilal544 <mughalzadaitx744@gmail.com>
2025-05-22 15:59:01 +0500

    home tool dummy functionality completed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,7 +46,7 @@
                     </div>
                 </div>
                 {{-- btn group + upload file btn end --}}
-                <div class="bottom mt-auto mb-3 px-4 flex items-center justify-between">
+                <div class="bottom mt-auto mb-3 px-4 pt-2 flex items-center justify-between border-t border-gray-200">
                     <div class="flex items-center gap-4 flex-1">
                         <div class="word-counter flex items-center" data-tooltip="Words">
                             <span class="text-gray-600 text-sm/normal font-normal js-entered-words">0</span>
@@ -70,7 +70,49 @@
             {{-- input-box-end --}}
 
             {{-- output box start --}}
-            <div class="output-box h-full flex-1 ps-4"> right box</div>
+            <div class="output-box h-full flex-1">
+                <div class="hidden js-loader flex-col gap-2 items-center justify-center min-h-[435px]">
+                    <span class="text-gray-600 text-sm/5 font-medium">Please wait...</span>
+                    <div class="loader"></div>
+                </div>
+                <div class="px-4 py-5 w-full flex-grow js-result-show">
+                    <textarea name="result-text" id="js-result-text" placeholder="Result will be display here..."
+                        spellcheck="false" readonly
+                        class="min-h-[380px] w-full resize-none border-0 ring-0 outline-0 focus:outline-0 focus:border-0 focus:ring-0"></textarea>
+                </div>
+                <div
+                    class="js-output-bottom mt-auto mb-3 px-4 pt-2 hidden items-center justify-between border-t border-gray-200">
+                    <div class="word-counter flex items-center" data-tooltip="Words">
+                        <span class="text-gray-600 text-sm/normal font-normal js-output-words">0</span>
+                    </div>
+                    <div class="flex items-center justify-end gap-2 flex-1">
+                        <button type="button" data-tooltip="Clear All" aria-label="delete-text"
+                            id="js-delete-result-text" class="flex cursor-pointer tooltip relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="size-5 text-gray-600">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                            </svg>
+                        </button>
+                        <button type="button" data-tooltip="Copy" data-copy="" aria-label="copy-text"
+                            id="js-copy-result-text" class="flex cursor-pointer tooltip relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="size-5 text-gray-500">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+                            </svg>
+                        </button>
+                        <button type="button" data-tooltip="Download" data-download="" aria-label="download-text"
+                            id="js-download-result-text" class="flex cursor-pointer tooltip relative">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" class="size-5 text-gray-500">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
             {{-- output box end --}}
         </div>
     </div>
--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,7 +5,30 @@
   @include('layout.head')
 </head>
 
-<body class="h-full max-w-full w-full overflow-x-hidden font-nunito">
+<body class="h-full max-w-full w-full overflow-x-hidden font-nunito relative">
+  {{-- popup start --}}
+  <div
+    class="js-popup absolute inset-0 px-3 py-2 hidden items-center justify-center w-full min-h-screen bg-gray-300/40 z-[999]">
+    <div class="popup-inner bg-white h-auto w-auto shadow rounded-md relative">
+      <button type="button" class="flex justify-end w-full cursor-pointer group py-2 px-3">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="size-6 text-gray-600 group-hover:text-gray-800 transition-all ease-in duration-200">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+      </button>
+      <div class="flex items-center flex-col gap-2.5 py-5 px-4">
+        <img src="{{asset('assets/images/not-found.jpg')}}" alt="img-not-found"
+          class="h-[200px] w-[200px] object-cover mix-blend-multiply">
+        <h2 class="text-red-600 text-4xl/7 font-semibold text-center">
+          No Text Found!
+        </h2>
+        <p class="text-gray-600 text-lg/6 font-normal text-center">
+          At least Enter 15 words to summarize text.
+        </p>
+      </div>
+    </div>
+  </div>
+  {{-- popup end--}}
   @include('layout.navbar')
   @yield('section')
   @include('layout.footer')

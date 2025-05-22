d54020c7e5714405a9c22055172512e20080452f
bilal544 <mughalzadaitx744@gmail.com>
2025-05-22 15:59:01 +0500

    home tool dummy functionality completed

--- public/assets/js/general.js
+++ public/assets/js/general.js
@@ -1,8 +1,18 @@
+const imageNotFound = `${BASE_URL}/assets/images/not-found.jpg?v=${version}`;
 const scriptsLinks = {
     jsPdf: "",
     jsZip: "",
     jsDocx: "",
+    jsAxios: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",
 };
+
+(() => {
+    $(document).ready(() => {
+        $(document).on("click", ".js-remove-popup", () => {
+            $(".js-popup").empty().removeClass("flex").addClass("hidden");
+        });
+    });
+})();
 
 const loadScript = async (scriptKey) => {
     const getScript = scriptsLinks[scriptKey];
@@ -37,4 +47,76 @@ const handleWordCounter = (text = "") => {
     return words.length;
 };
 
-export { handleWordCounter, loadScript };
+const popup = (
+    title = "No Text Found!",
+    text = " At least Enter 15 words to summarize text."
+) => {
+    $(".js-popup").empty();
+    const $html = `
+        <div class="popup-inner bg-white h-auto w-auto shadow rounded-md relative">
+      <button type="button" class="flex justify-end w-full cursor-pointer group py-2 px-3 js-remove-popup">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+          class="size-6 text-gray-600 group-hover:text-gray-800 transition-all ease-in duration-200">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+        </svg>
+      </button>
+      <div class="flex items-center flex-col gap-2.5 py-5 px-4">
+        <img src="${imageNotFound}" alt="img-not-found"
+          class="h-[200px] w-[200px] object-cover mix-blend-multiply">
+        <h2 class="text-red-600 text-4xl/7 font-semibold text-center">
+          ${title}
+        </h2>
+        <p class="text-gray-600 text-lg/6 font-normal text-center">
+          ${text}
+        </p>
+      </div>
+    </div>
+    `;
+
+    $(".js-popup").html($html).removeClass("hidden").addClass("flex");
+};
+
+const handleCopyResult = (e) => {
+    const targetText = $(e.currentTarget).data("copy");
+    $(e.currentTarget).attr("data-tooltip", "Copied!");
+
+    if (targetText) {
+        navigator.clipboard
+            .writeText(targetText)
+            .then(() => {
+                setTimeout(() => {
+                    $(e.currentTarget).attr("data-tooltip", "Copy");
+                }, 1500);
+            })
+            .catch((error) => {
+                console.error("No text copied:", error);
+            });
+    }
+};
+
+const handleDownloadResult = (e) => {
+    const targetText = $(e.currentTarget).data("download");
+    $(e.currentTarget).attr("data-tooltip", "Downloading...");
+    if (targetText) {
+        const blob = new Blob([targetText], { type: "text/plain" });
+        const url = URL.createObjectURL(blob);
+
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "summarize.txt";
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+
+        URL.revokeObjectURL(url);
+        $(e.currentTarget).attr("data-tooltip", "Download");
+    }
+};
+
+export {
+    handleWordCounter,
+    loadScript,
+    popup,
+    handleCopyResult,
+    handleDownloadResult,
+};
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,5 +1,6 @@
-import { handleWordCounter } from "./general.js";
+import { handleCopyResult, handleDownloadResult, handleWordCounter, loadScript, popup } from "./general.js";
 
+const API_BASE_URL = "https://jsonplaceholder.typicode.com/";
 const MAX_WORDS = 500;
 const MIN_WORDS = 15;
 const sampleText =
@@ -19,6 +20,13 @@ const sampleText =
         $("#js-sample-text").on("click", handleSampleText);
         $("#js-delete-text").on("click", handleDeleteText);
         $("#js-paste-text").on("click", handlePasteText);
+        $(".js-text-summarize").on("click", handleSummarize);
+        $(document).on("click", "#js-copy-result-text", handleCopyResult);
+        $(document).on(
+            "click",
+            "#js-download-result-text",
+            handleDownloadResult
+        );
     });
 
     // handle input when user type text in textarea
@@ -29,11 +37,27 @@ const sampleText =
         if (trimText) {
             $("#js-btn-group").removeClass("flex").addClass("hidden");
             $("#js-delete-text").removeClass("hidden").addClass("block");
-            $(".js-entered-words").text(words);
+            $(".js-entered-words")
+                .text(words)
+                .removeClass("text-red-600 text-gray-600")
+                .addClass("text-gray-600");
+            if (words > MAX_WORDS) {
+                popup(
+                    "Text Too Long!",
+                    "Only 500 words are accepted to summarize."
+                );
+                $(".js-entered-words")
+                    .addClass("text-red-600")
+                    .removeClass("text-gray-600");
+                return;
+            }
         } else {
             $("#js-btn-group").removeClass("hidden").addClass("flex");
             $("#js-delete-text").removeClass("block").addClass("hidden");
-            $(".js-entered-words").text(0);
+            $(".js-entered-words")
+                .text(0)
+                .removeClass("text-red-600 text-gray-600")
+                .addClass("text-gray-600");
         }
     };
 
@@ -60,5 +84,69 @@ const sampleText =
             .catch((err) => {
                 console.error("Failed to read clipboard: ", err);
             });
+    };
+
+    // handle summarize text
+    const handleSummarize = async () => {
+        const btnText = $(".js-text-summarize").text();
+        const inputText = $("#js-input-text").val().trim() || "";
+        const words = handleWordCounter(inputText);
+
+        if (inputText == "" || words < MIN_WORDS) {
+            popup();
+            return;
+        }
+
+        if (words > MAX_WORDS) {
+            popup(
+                "Text Too Long!",
+                "Only 500 words are accepted to summarize."
+            );
+            return;
+        }
+
+        // load axios script
+        await loadScript("jsAxios");
+        $(".js-text-summarize")
+            .text("Summarizing...")
+            .addClass("pointer-events-none bg-[#131313]/50")
+            .removeClass("cursor-pointer");
+        $(".js-result-show").addClass("hidden");
+        $(".js-loader").addClass("flex").removeClass("hidden");
+        $(".js-output-bottom").addClass("hidden").removeClass("flex");
+        try {
+            const response = await axios.post(
+                `${API_BASE_URL}/posts`,
+                {
+                    text: inputText,
+                },
+                {
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                }
+            );
+
+            if (response) {
+                $(".js-loader").removeClass("flex").addClass("hidden");
+                const resultText = response?.data?.text || "No result found.";
+                const outputWords = handleWordCounter(resultText.trim());
+                $(".js-output-bottom").removeClass("hidden").addClass("flex");
+                $("#js-result-text").val(resultText);
+                $(".js-output-words").text(outputWords);
+                $(".js-result-show").removeClass("hidden");
+                $(".js-text-summarize")
+                    .text(btnText)
+                    .removeClass("pointer-events-none bg-[#131313]/50")
+                    .addClass(
+                        "cursor-pointer pointer-events-auto bg-[#131313]"
+                    );
+                $("#js-download-result-text").attr("data-download", resultText);
+                $("#js-copy-result-text").attr("data-copy", resultText);
+            }
+        } catch (error) {
+        } finally {
+            $(".js-loader").removeClass("flex").addClass("hidden");
+        }
     };
 })();
